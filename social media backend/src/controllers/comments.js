bc440e2598001bc3729807cf856e0511254b0475
Tanishq Patidar <tanishq.patidar@kalvium.community>
2025-08-24 19:13:31 +0530

    feat(comments): update createCommentController to validate post existence and comment permissions

--- social media backend/src/controllers/comments.js
+++ social media backend/src/controllers/comments.js
@@ -4,6 +4,7 @@ const {
   deleteComment,
   getCommentsByPostId,
 } = require("../models/comment");
+const { getPostById } = require("../models/post");
 const logger = require("../utils/logger");
 
 /**
@@ -12,8 +13,17 @@ const logger = require("../utils/logger");
 const createCommentController = async (req, res) => {
   try {
     const userId = req.user.id;
-    const { post_id } = req.params;
+    const { post_id } = req.body;
     const { content } = req.body;
+    const post = await getPostById(parseInt(post_id));
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    if (post.comments_enabled === false) {
+      return res
+        .status(403)
+        .json({ error: "Comments are disabled for this post" });
+    }
     const comment = await createComment({
       user_id: userId,
       post_id: parseInt(post_id),
