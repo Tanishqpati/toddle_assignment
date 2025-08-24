18c1e66148d91121ebd767edb667a0ab88fe780e
Tanishq Patidar <tanishq.patidar@kalvium.community>
2025-08-24 17:34:08 +0530

    feat(comments): implement comment routes for creating, updating, deleting, and retrieving comments

--- social media backend/src/routes/comments.js
+++ social media backend/src/routes/comments.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const { authenticateToken } = require("../middleware/auth");
+const {
+  createComment,
+  updateComment,
+  deleteComment,
+  getPostComments,
+} = require("../controllers/comments");
 
 const router = express.Router();
 
@@ -8,9 +14,16 @@ const router = express.Router();
  * TODO: Implement comment routes when comment functionality is added
  */
 
-// TODO: POST /api/comments - Create a comment on a post
-// TODO: PUT /api/comments/:comment_id - Update a comment
-// TODO: DELETE /api/comments/:comment_id - Delete a comment
-// TODO: GET /api/comments/post/:post_id - Get comments for a post
+// POST /api/comments - Create a comment on a post (expects post_id in body)
+router.post("/", authenticateToken, createComment);
+
+// PUT /api/comments/:comment_id - Update a comment
+router.put("/:comment_id", authenticateToken, updateComment);
+
+// DELETE /api/comments/:comment_id - Delete a comment
+router.delete("/:comment_id", authenticateToken, deleteComment);
+
+// GET /api/comments/post/:post_id - Get comments for a post
+router.get("/post/:post_id", getPostComments);
 
 module.exports = router;
