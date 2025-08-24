bf595ac2bb86f6fc9a1228638c07688b82469826
Tanishq Patidar <tanishq.patidar@kalvium.community>
2025-08-24 15:36:24 +0530

    feat(likes): implement like routes for liking, unliking, and retrieving post likes

--- social media backend/src/routes/likes.js
+++ social media backend/src/routes/likes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const { authenticateToken } = require("../middleware/auth");
+const {
+  likePost,
+  unlikePost,
+  getPostLikes,
+  getUserLikes,
+} = require("../controllers/likes");
 
 const router = express.Router();
 
@@ -8,9 +14,16 @@ const router = express.Router();
  * TODO: Implement like routes when like functionality is added
  */
 
-// TODO: POST /api/likes - Like a post
-// TODO: DELETE /api/likes/:post_id - Unlike a post
-// TODO: GET /api/likes/post/:post_id - Get likes for a post
-// TODO: GET /api/likes/user/:user_id - Get posts liked by a user
+// POST /api/likes - Like a post
+router.post("/", authenticateToken, likePost);
+
+// DELETE /api/likes/:post_id - Unlike a post
+router.delete("/:post_id", authenticateToken, unlikePost);
+
+// GET /api/likes/post/:post_id - Get likes for a post
+router.get("/post/:post_id", getPostLikes);
+
+// GET /api/likes/user/:user_id - Get posts liked by a user
+router.get("/user/:user_id", getUserLikes);
 
 module.exports = router;
